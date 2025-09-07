1a300b1b91baa0f08b36c4c6c2a10cf25f177c9b
singhravi5590 <singhravi5590@gmail.com>
2025-09-07 12:46:04 +0530

    Navbar added

---
+++ src/components/Navigation/FullScreenNav.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+
+const FullScreenNav = () => {
+  return (
+    <div id='fullscreennav' className='h-screen w-full absolute bg-amber-900'>
+        <div id='all-links'>
+            <div>
+                <div>
+                    <h1>Projects</h1>
+                    <div>
+                        <h2>Pour Tour Voir</h2>
+                        <img src="https://k72.ca/uploads/caseStudies/WIDESCAPE/WS---K72.ca---MenuThumbnail-640x290.jpg" alt="" />
+                        <h2>Pour Tour Voir</h2>
+                        <img src="https://k72.ca/uploads/caseStudies/PJC/Thumbnails/PJC_SiteK72_Thumbnail_640x290-640x290.jpg" alt="" />
+                    </div>
+                    <div>
+                        <h2>Pour Tour Voir</h2>
+                        <img src="https://k72.ca/uploads/caseStudies/WIDESCAPE/WS---K72.ca---MenuThumbnail-640x290.jpg" alt="" />
+                        <h2>Pour Tour Voir</h2>
+                        <img src="https://k72.ca/uploads/caseStudies/PJC/Thumbnails/PJC_SiteK72_Thumbnail_640x290-640x290.jpg" alt="" />
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default FullScreenNav
